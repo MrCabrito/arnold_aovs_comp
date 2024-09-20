44c5f8658d749f5c30945fdd6b6e2578a6fc516a
Abraham Gonzalez <abraham.gocape@gmail.com>
2024-09-20 13:39:34 -0600

    Updated UI
    Updated UI in Qt designer

---
+++ UI/UI_arnold_aov_comp_ui.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'f:\Projects\PROGRAMMING\Repos\arnold_aovs_gizmos\UI\UI_arnold_aov_comp.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(295, 55)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setMinimumSize(QtCore.QSize(208, 55))
+        Form.setMaximumSize(QtCore.QSize(16777215, 16777215))
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setSpacing(2)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(5, 5, 5, 5)
+        self.horizontalLayout.setSpacing(5)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.widget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.cBox_template = QtWidgets.QComboBox(self.widget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.cBox_template.sizePolicy().hasHeightForWidth())
+        self.cBox_template.setSizePolicy(sizePolicy)
+        self.cBox_template.setObjectName("cBox_template")
+        self.cBox_template.addItem("")
+        self.cBox_template.addItem("")
+        self.cBox_template.addItem("")
+        self.horizontalLayout.addWidget(self.cBox_template)
+        self.verticalLayout.addWidget(self.widget)
+        self.widget_2 = QtWidgets.QWidget(Form)
+        self.widget_2.setObjectName("widget_2")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.widget_2)
+        self.horizontalLayout_2.setContentsMargins(5, 0, 5, 0)
+        self.horizontalLayout_2.setSpacing(2)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.chBox_new_group = QtWidgets.QCheckBox(self.widget_2)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.chBox_new_group.sizePolicy().hasHeightForWidth())
+        self.chBox_new_group.setSizePolicy(sizePolicy)
+        self.chBox_new_group.setObjectName("chBox_new_group")
+        self.horizontalLayout_2.addWidget(self.chBox_new_group)
+        spacerItem = QtWidgets.QSpacerItem(31, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.btn_create = QtWidgets.QPushButton(self.widget_2)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btn_create.sizePolicy().hasHeightForWidth())
+        self.btn_create.setSizePolicy(sizePolicy)
+        self.btn_create.setObjectName("btn_create")
+        self.horizontalLayout_2.addWidget(self.btn_create)
+        self.verticalLayout.addWidget(self.widget_2)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "AOV Template:"))
+        self.cBox_template.setItemText(0, _translate("Form", "Simple"))
+        self.cBox_template.setItemText(1, _translate("Form", "Intermediate"))
+        self.cBox_template.setItemText(2, _translate("Form", "Complex"))
+        self.chBox_new_group.setText(_translate("Form", "Create a new group"))
+        self.btn_create.setText(_translate("Form", "Ok"))
