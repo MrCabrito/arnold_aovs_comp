4320d9fbe5cbb371b7b48231c96ad162edcf8a1a
Abraham Gonzalez <abraham.gocape@gmail.com>
2024-10-04 21:14:40 -0600

    Updated sanity checks
    Updated text to display and node color for errors found

--- utilities/sanity_check.py
+++ utilities/sanity_check.py
@@ -14,10 +14,11 @@ def AOV_check(layers_dict: dict) -> dict|None:
     read_data = dict()
     wrong_data = dict()
     # Start progress bar
-    task = create_progress_task('Searching for correct passes in the read nodes selected')
+    task = create_progress_task('Searching for correct AOVs in the read nodes selected')
     progPerRead = 90.0/float(len(read_nodes))
     progress = 10
     for read_node in read_nodes:
+        read_node.knob('tile_color').setValue(0)
         missing_layers = list()
         layers_found = dict()
         task.setMessage('Reviewing {}'.format(read_node['name'].value()))
@@ -35,7 +36,7 @@ def AOV_check(layers_dict: dict) -> dict|None:
             layers_found[group] = layers
         # If there are missing AOV's group them with the corresponded read node for future error message
         if missing_layers:
-            wrong_data[read_node['name'].value()] = missing_layers
+            wrong_data[read_node] = missing_layers
         # If there is no issue added to the dictionary
         else:
             read_data[read_node] = layers_found
@@ -46,9 +47,11 @@ def AOV_check(layers_dict: dict) -> dict|None:
             return None
     if wrong_data:
         # Creates an error message for founded missing AOV's 
-        error_message = 'There are some missing passes\n'
+        error_message = 'There are some missing AOVs\n'
+        nuke_hex = int('%02x%02x%02x%02x' % (int(1*255),int(0*255),int(0*255),255),16)
         for read_node, missing_layers in wrong_data.items():
-            error_message += '{0} -> {1}\n'.format(read_node, ', '.join(missing_layers))
+            error_message += '{0} -> {1}\n'.format(read_node['name'].value(), ', '.join(missing_layers))
+            read_node.knob('tile_color').setValue(nuke_hex)
         error_messages(error_message)
         return None
     
